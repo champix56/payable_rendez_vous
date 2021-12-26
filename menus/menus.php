1f70ad7e0355706f239d4411b0fcdbb4c4c44314
champix56 <desorbaix@free.fr>
2021-12-27 00:00:08 +0100

    enqueue script & style

--- menus/menus.php
+++ menus/menus.php
@@ -1,5 +1,7 @@
 <?php
+    
     include_once($PLUGIN_DIR."menus/client/client.php");
     include_once($PLUGIN_DIR."menus/consultant/consultant.php");
     include_once($PLUGIN_DIR."menus/admin/admin.php");
+    add_action('admin_enqueue_scripts', 'enqueue_consultant_menu_script');
 ?>
