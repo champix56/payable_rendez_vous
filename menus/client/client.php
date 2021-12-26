0c5ea2c108f63900542e230ac2b7196d1a074f29
champix56 <desorbaix@free.fr>
2021-12-26 17:07:39 +0100

    add take apointment model

--- menus/client/client.php
+++ menus/client/client.php
@@ -1,9 +1,44 @@
 <?php
+include_once($PLUGIN_DIR."core/users.php");
 function rdv_menu_client_histo(){
     ?><h1>Mon historique</h1><?php
 }
 function rdv_menu_client_take_apointment(){
-    ?><h1>Prendre rendez vous</h1><?php
+    $users = get_consultants();
+    //print_r($users);
+    
+    ?><h1>Prendre rendez vous</h1>
+    <form>
+    <label>Selectionner consultant :</label><br/>
+    <select><?php
+    foreach ($users as $value){
+        ?><option value="<?php echo $value->ID ;?>"><?php echo $value->data->display_name ;?></option>
+      <?php
+    }?>
+    </select>
+    <br/>
+    <label>Selectionnez date : <br/><input type="date" name="" id=""></label>
+    <br/>
+
+    <label>Selectionnez heure : <br/><input type="time" name="" id=""></label>
+    <br/>
+<label for="">Selectionnez une prestation : </label><br/>
+<select name="" id="">
+    <optgroup label="Horaire">
+        <option value="h">1h/35euros</option>
+        <option value="h">30Min/20euros</option>
+    </optgroup>
+    <optgroup label="questions">
+        <option value="h">3 questions/35euros</option>
+    </optgroup>
+</select>
+<br/><br/><br/>
+<div style="display: flex;justify-content: space-around;"></div>
+<button style="padding: 5px 15px; border-radius: 7px; background-color: skyblue; color:white; border:none; " type="submit">Valider</button>
+<button style="padding: 5px 15px; border-radius: 7px; background-color: tomato; color:white; border:none; "  type="reset">Annuler</button>
+</form>
+    <?php
+
 }
 function rdv_menu_client_base_page(){
     ?><h1>Espace client</h1><?php
