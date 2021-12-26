7d08e7a17da02f3e814b7bbd624157da3f2d7061
champix56 <desorbaix@free.fr>
2021-12-26 17:06:27 +0100

    make log +add db install management

--- install/install.php
+++ install/install.php
@@ -1,13 +1,21 @@
 <?php
 include_once($PLUGIN_DIR."install/roles/roles.php");
+include_once($PLUGIN_DIR."install/db/db.php");
 $DEBUG;
 function rdv_install_all()
 {
-   global $DEBUG;
-   rdv_install_roles(true);
+   error_log("_______________\n\t\t\tBegin install\n");
+   rdv_install_roles();
+   rdv_install_db();
+   error_log("\n\t\t\tEnd install\n_______________\n");
+
 }
-function rdv_unistall_all()
+function rdv_uninstall_all()
 {
+   error_log("_______________\n\t\t\tBegin uninstall\n");
    rdv_uninstall_roles();
+   rdv_uninstall_db();
+   error_log("\n\t\t\tEnd uninstall\n_______________\n");
+
 }
 ?>
