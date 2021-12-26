f50789c0c16818cab08df750afa42771a448ad11
champix56 <desorbaix@free.fr>
2021-12-26 17:05:26 +0100

    add global values

--- constant.php
+++ constant.php
@@ -1,7 +1,15 @@
 <?php
+global $wpdb;
+    global $DBG;
     $DBG=true;
+    global $PLUGIN_SLUG;
     $PLUGIN_SLUG="PAYABLE_RDV";
+    global $PLUGIN_DIR;
     $PLUGIN_DIR=plugin_dir_path(__FILE__);
+    global $PLUGIN_BASE_URL;
     $PLUGIN_BASE_URL=plugin_dir_url( __DIR__ );
+    global $PLUGIN_ERROR_FILE;
     $PLUGIN_ERROR_FILE=$PLUGIN_DIR."/WPERROR.log";
+    global $PLUGIN_DB_PREFIX;
+    $PLUGIN_DB_PREFIX=$wpdb->prefix."rdv_";
 ?>
