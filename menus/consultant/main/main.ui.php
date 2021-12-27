8d886a142db1c6d8a3500622388f2f2def4eab64
champix56 <desorbaix@free.fr>
2021-12-27 17:27:03 +0100

    change force radius

--- menus/consultant/main/main.ui.php
+++ menus/consultant/main/main.ui.php
@@ -2,7 +2,7 @@
 function get_vignette_rdv($clientID, $clientName,$date, $time, $presta){
     ?>
         <div class="vignet_consultant_future_apointement" style="text-align: center; min-width: 140px;;">
-            <img src="<?= get_avatar_url($clientID)?>" alt="" style="border-radius:50%"><br/>
+            <img src="<?= get_avatar_url($clientID)?>" alt="" style="border-radius:35px"><br/>
             <?= $clientName?><br/>
             <?= $date?>&nbsp;<?= $time?><br/>
             <?= $presta?><br/>
