6392bdd971b7c82744c2a25daeabbe377a280f9c
champix56 <desorbaix@free.fr>
2021-12-27 17:26:45 +0100

    add insert availabilty

--- menus/consultant/availabilities/availabilities.func.php
+++ menus/consultant/availabilities/availabilities.func.php
@@ -1,12 +1,13 @@
 <?php
-    function add_consultant_availability()
+    function add_consultant_availability($dayOfWeek,$start,$end)
     {
         global $wpdb;
         global $PLUGIN_DB_PREFIX;
         $me=wp_get_current_user();
-        $sql="SELECT * FROM {$PLUGIN_DB_PREFIX}consultant_availabilities WHERE consultantID={$me->data->ID}";
-        return $sql;
-    }
+        $sql="SINSERT INTO {$PLUGIN_DB_PREFIX}consultant_availabilities (`consultantID`, `day_of_week`, `time_start`, `time_end`) VALUES ($me->data->ID}, {$dayOfWeek}, '{$start}', '{$end}');";
+ 
+        return $wpdb->query($sql);
+        }
     function list_consultant_availability(){
         global $wpdb;
         global $PLUGIN_DB_PREFIX;
