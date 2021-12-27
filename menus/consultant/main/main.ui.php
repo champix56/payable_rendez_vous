ec2ae48235c86d41934b68ac68e30b38bc37dc2b
champix56 <desorbaix@free.fr>
2021-12-27 20:30:18 +0100

    passage par class css

--- menus/consultant/main/main.ui.php
+++ menus/consultant/main/main.ui.php
@@ -1,8 +1,8 @@
 <?php 
 function get_vignette_rdv($clientID, $clientName,$date, $time, $presta){
     ?>
-        <div class="vignet_consultant_future_apointement" style="text-align: center; min-width: 140px;;">
-            <img src="<?= get_avatar_url($clientID)?>" alt="" style="border-radius:35px"><br/>
+        <div class="vignet_consultant_future_apointement">
+            <img class="avatar" src="<?= get_avatar_url($clientID)?>" alt="" ><br/>
             <?= $clientName?><br/>
             <?= $date?>&nbsp;<?= $time?><br/>
             <?= $presta?><br/>
