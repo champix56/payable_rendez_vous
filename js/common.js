57c40bb891505e029b7ad162e3af8429990908f1
champix56 <desorbaix@free.fr>
2021-12-29 02:07:08 +0100

    js common functions

---
+++ js/common.js
@@ -0,0 +1,38 @@
+/**
+ * 
+ * @param {string} adr adress to fetch
+ * @param {GET|POST|DELETE|PUT|PATCH|OPTION} method fetch http method
+ * @param {string} body stringified content
+ * @returns Promise
+ */
+function wpAdminFetch(adr,method='GET',body=undefined){
+    return fetch(adminMenuBaseUrl()+adr,{
+        headers:{
+            'X-WP-Nonce':wpApiSettings.nonce
+        },
+        body:body,
+        method:method
+    });
+}
+function adminMenuBaseUrl(){
+    const fullAdr=location.href;
+    return fullAdr.substring(0,fullAdr.indexOf('/wp-admin'));
+
+}/**
+ * name of day
+ * @param {number} dayOfWeekNumber 
+ * @returns string
+ */
+function getNameOfDay(dayOfWeekNumber) {
+    switch (dayOfWeekNumber) {
+        case 1:return 'Lundi';
+        case 2:return 'Mardi';
+        case 3:return 'Mercredi';
+        case 4:return 'Jeudi';
+        case 5:return 'Vendredi';
+        case 6:return 'Samedi';
+        case 7:return 'Dimanche';
+        default:
+            return '';
+    }
+}
