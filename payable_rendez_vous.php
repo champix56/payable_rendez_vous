66d989e9428306d6df1048d6d27a7d4970a0f904
champix56 <desorbaix@free.fr>
2021-12-29 02:08:16 +0100

    add coomon script && api functions

--- payable_rendez_vous.php
+++ payable_rendez_vous.php
@@ -10,8 +10,12 @@
 include_once(plugin_dir_path( __FILE__).'constant.php');
 include($PLUGIN_DIR.'/install/install.php');
 include($PLUGIN_DIR.'/menus/menus.php');
+include($PLUGIN_DIR.'/api/api.php');
 register_activation_hook(__FILE__, 'rdv_install_all');
 register_deactivation_hook(__FILE__, 'rdv_uninstall_all');
-
-
+add_action('admin_menu',function(){
+    global $PLUGIN_BASE_URL;
+    wp_enqueue_script( 'wp-api' );
+    wp_enqueue_script( 'common-wp-api-js', $PLUGIN_BASE_URL."js/common.js", ['wp-api']);
+})
 ?>
