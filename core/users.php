4a27f2ca13e996446e5d61c771e410593d928801
champix56 <desorbaix@free.fr>
2021-12-29 02:19:09 +0100

    add get users data functions

--- core/users.php
+++ core/users.php
@@ -15,3 +15,41 @@ function get_consultants($andAdmin = true)
     }
     return $specific_users;
 }
+function get_consultants_unavailabilities($id)
+{
+    global $wpdb;
+    global $PLUGIN_DB_PREFIX;
+    $sql="SELECT * FROM `{$PLUGIN_DB_PREFIX}consultant_unavailabilities` WHERE `consultantID`={$id} ORDER BY `start`";
+    return $wpdb->get_results($sql);
+    // return $specific_users;
+}
+function get_consultants_availabilities($id)
+{
+    global $wpdb;
+    global $PLUGIN_DB_PREFIX;
+    $sql="SELECT * FROM `{$PLUGIN_DB_PREFIX}consultant_availabilities` WHERE `consultantID`={$id} ORDER BY `day_of_week`, `time_start`";
+    return $wpdb->get_results($sql);
+    // return $specific_users;
+}/**
+ * get user roles by ID
+ *
+ * @param [number] $user_id
+ * @return array
+ */
+function get_user_roles_by_user_id( $user_id ) {
+   // echo "id;";
+    $user = get_userdata( $user_id );
+    return empty( $user ) ? array() : $user->roles;
+}
+/**
+ * check role of user
+ *
+ * @param [number] $user_id
+ * @param [string] $role
+ * @return boolean
+ */
+function is_user_in_role( $user_id, $role  ) {
+    //echo "role;";
+    return in_array( $role, get_user_roles_by_user_id( $user_id ) );
+}
+?>
