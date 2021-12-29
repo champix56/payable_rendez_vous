2fd0cfb2a4ae4a8e170293cfbd42ba6ce8608237
champix56 <desorbaix@free.fr>
2021-12-29 02:11:39 +0100

    menu take apointement with consult ajax loading

--- menus/client/take_apointment/take_apointment.php
+++ menus/client/take_apointment/take_apointment.php
@@ -7,18 +7,26 @@ $consultants = get_consultants();
         <div class="flex center-child" style="justify-content: space-around;">
             <div>
                 <label>Selectionner consultant :</label><br />
-                <select><?php
+                <select id="client_take_apointment_select_consultant"><?php
                         foreach ($consultants as $value) {
                         ?><option value="<?php echo $value->ID; ?>"><?php echo $value->data->display_name; ?></option>
                     <?php
                         } ?>
                 </select>
+                <div class="consultant-viewer">
+                    <img src="" alt="">
+                    <div class="infos"></div>
+                </div>
             </div>
             <div>
-                <label>Selectionnez date : <br /><input type="date" name="" id=""></label>
-                <br />
+            <div id="calendar"></div>
+        </div>
+            <!-- <div>
+                <!- - <label>Selectionnez date : <br /><input type="date" name="" id=""></label> 
+                <br />- ->
+                
                 <label>Selectionnez heure : <br /><input type="time" name="" id=""></label>
-            </div>
+            </div> -->
             <div>
                 <label for="">Selectionnez une prestation : </label><br />
                 <select name="" id="">
@@ -33,6 +41,7 @@ $consultants = get_consultants();
             </div>
         </div>
         <hr />
+     
         <div class="flex" style="justify-content: space-around;">
             <button style="padding: 5px 15px; border-radius: 7px; background-color: tomato; color:white; border:none; " type="reset">Annuler</button>
             <button style="padding: 5px 15px; border-radius: 7px; background-color: skyblue; color:white; border:none; " type="submit">Valider</button>
